89afd3042d0b11fede1fbf38fc0c7afa7c6b7336
Kirill Kontorovich <Kirill Kontorovich>
2022-12-04 22:58:03 +0500

    fixes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,36 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
+const findRandomChild = (playerId) => {
+  const db = JSON.parse(fs.readFileSync(`${__dirname}/../db.json`));
+  const playerIndex = db.players.findIndex(({ id, isSanta }) => id === Number(playerId) && !isSanta);
+  const player = db.players[playerIndex];
+
+  if (player) {
+
+    const loop = () => {
+      const secondPlayerIndex = Math.floor(Math.random() * db.players.length);
+      const secondPlayer = db.players[secondPlayerIndex]
+      if (secondPlayer.id !== player.id && !secondPlayer.isChild) {
+        db.players[secondPlayerIndex].isChild = true;
+        db.matches.push({ from: player.id, to: secondPlayer.id  })
+      } else {
+        loop();
+      }
+    }
+
+    loop();
+
+    db.players[playerIndex].isSanta = true;
+
+    fs.writeFileSync(`${__dirname}/../db.json`, JSON.stringify(db), 'utf-8')
+
+    return true;
+  }
+
+  return false;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const db = JSON.parse(fs.readFileSync(`${__dirname}/../db.json`));
@@ -11,23 +41,14 @@ router.get('/', function(req, res, next) {
     res.redirect('/your-child')
   }
 
-  res.render('index', { players: db.players });
+  res.render('index', { players: db.players.filter(({ isSanta }) => !isSanta) });
 });
 
 router.get('/become-santa', (req, res, next) => {
-  const db = JSON.parse(fs.readFileSync(`${__dirname}/../db.json`));
   const { playerId } = req.query;
   
   if (playerId) {
-    const playerIndex = db.players.findIndex(({ id }) => id === Number(playerId));
-    const player = db.players[playerIndex];
-    db.players.splice(playerIndex, 1);
-
-    const childIndex = Math.floor(Math.random() * db.players.length) 
-    db.matches.push({ from: player, to: db.players[childIndex]  })
-    db.players.splice(childIndex, 1);
-
-    fs.writeFileSync(`${__dirname}/../db.json`, JSON.stringify(db), 'utf-8')
+    findRandomChild(playerId)
 
     res.cookie('playerId', playerId, { httpOnly: true });
     res.redirect('/your-child')
@@ -42,9 +63,15 @@ router.get('/your-child', (req, res) => {
   const playerId = req.cookies.playerId;
 
   if (playerId) {
-    const match = db.matches.find(({ from: { id } }) => id === Number(playerId))
+    const match = db.matches.find(({ from }) => from === Number(playerId))
+    const secondPlayer = db.players.find(({ id }) => match?.to === id)
 
-    res.render('your-child', { child: match.to })
+    if (secondPlayer?.isChild) {
+      res.render('your-child', { child: secondPlayer })
+    } else {
+      res.clearCookie('playerId')
+      res.redirect('/');
+    }
   } else {
     res.redirect('/');
   }
